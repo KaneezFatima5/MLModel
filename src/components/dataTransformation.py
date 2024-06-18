744618f71f4d58c1ffc1eae4ad4cf469b71903e4
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-06-18 18:29:41 +0500

    data tranformation for train and test data sets

--- src/components/dataTransformation.py
+++ src/components/dataTransformation.py
@@ -57,6 +57,42 @@ class datatransformation:
 
             logging.info("Reading train and test files")
 
+            pre_processing_obj=self.get_data_transformer_object()
+
+            target_column_name="math_score"
+            numerical_columns=["writing_score", "reading_score"]
+
+            input_features_train_df=train_df.drop(columns=[target_column_name], axis=1)
+            target_features_train_df=train_df[target_column_name]
+
+
+            input_features_test_df=test_df.drop(columns=[target_column_name], axis=1)
+            target_features_test_df=test_df[target_column_name]
+
+            logging.info("Applying preprocessing into train and test dataframe")
+
+            input_feature_train_arr=pre_processing_obj.fit_transform(input_features_train_df)
+            input_features_test_arr=pre_processing_obj.fit(input_features_test_df)
+
+            train_arr=np.c_[
+                input_feature_train_arr, np.array(target_features_train_df)
+            ]
+            test_arr= np.c_[
+                input_features_test_arr, np.array(target_features_test_df)
+            ]
+
+            save_object(
+                file_path=self.dataTransformationConfig.processor_file_path,
+                obj=pre_processing_obj
+            )
+
+            return(
+                train_arr,
+                test_arr,
+                self.dataTransformationConfig.processor_file_path
+            )
+
+
         except Exception as e:
             raise CustomException(e, sys)
         
